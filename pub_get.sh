f22e3c96c313135d7f7560c17c96e93be74ac061
Micheal Aro <micheal.aro@etailer.de>
2023-04-25 17:47:22 +0200

    first v1.0.2

---
+++ pub_get.sh
@@ -0,0 +1,10 @@
+cd webview_flutter  
+flutter pub get
+cd webview_flutter_android  
+flutter pub get
+cd webview_flutter_platform_interface  
+flutter pub get
+cd webview_flutter_web  
+flutter pub get
+cd webview_flutter_wkwebview
+flutter pub get

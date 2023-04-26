10894f374d5c6c8dcf256fea8957e64d2031abce
Micheal Aro <micheal.aro@etailer.de>
2023-04-26 09:39:04 +0200

    reset

--- webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/GeoLocationPermissionRequestInterface.java
+++
@@ -1,7 +0,0 @@
-package io.flutter.plugins.webviewflutter;
-
-public interface GeoLocationPermissionRequestInterface {
-
-    public abstract void onGrant(boolean allow, boolean retain);
-    public abstract void onDeny();
-}
